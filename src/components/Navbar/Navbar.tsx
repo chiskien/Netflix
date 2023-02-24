5177d477f2e966a6c6e5cfe5822d0df75a202173
ChisKien <chiskien03214@gmail.com>
2023-02-24 15:15:01 +0700

    avatar image

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.scss"
+
+export const Navbar: React.FC = () => {
+    return (
+        <div className="nav">
+            <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+                 alt="Netflix logo"/>
+            <img src="" alt=""/>
+        </div>
+    )
+}
