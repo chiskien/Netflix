874a4027b157a7464253261d2203d534f2354be5
ChisKien <chiskien03214@gmail.com>
2023-02-24 10:43:06 +0700

    netflix initialize

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiia39StQ8AdUWEFR1Ix5jU6GrM_q1Yyg",
+    authDomain: "netflix-bd782.firebaseapp.com",
+    projectId: "netflix-bd782",
+    storageBucket: "netflix-bd782.appspot.com",
+    messagingSenderId: "727263130019",
+    appId: "1:727263130019:web:6f84a87503963d8c216a16"
+};
