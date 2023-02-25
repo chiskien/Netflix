e81595a18d72a2340ff498c1f535d66ee9f590e2
ChisKien <chiskien03214@gmail.com>
2023-02-25 20:17:36 +0700

    navbar components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Navbar.scss"
+import "./Navbar.scss";
 
 export const Navbar: React.FC = () => {
     return (
         <div className="nav">
             <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
                  alt="Netflix logo"/>
-            <img src="" alt=""/>
+            <img src="~/../peep-101.svg" alt="avatar"/>
         </div>
     )
 }
