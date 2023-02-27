87b3de7bc1747f1600765ba41e426dc1c52985eb
ChisKien <chiskien03214@gmail.com>
2023-02-27 11:38:09 +0700

    Models's State

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,21 +1,23 @@
+import axios, {AxiosRequestConfig, InternalAxiosRequestConfig} from "axios";
+import {Movie} from "../models/Movie";
+
 export const tmdbApi: string = "https://api.themoviedb.org/3";
 
 const key: string = "2a14a0070cbbea8bb85612746901228a";
 
-const fetchConfig: RequestInit = {
-    mode: "no-cors",
-    method: "get",
-}
+const axiosClient = axios.create({
+    baseURL: tmdbApi
+})
 
-export async function getLatestMovies() {
-    const response = await fetch(`${tmdbApi}/movie/latest?api_key=${key}`);
-    return await response.json();
-}
+axiosClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    config.baseURL = tmdbApi;
+    config.method = "GET";
+    config.params = config.params || {};
+    config.params["api_key"] = key;
+    return config;
+})
 
-export async function getGenres() {
-    const response = await fetch(`${tmdbApi}/genre/movie/list`, {
-        method: "get",
-        mode: "no-cors"
-    });
-
+export const getPopularMovies = async (page = 1) => {
+    const response = await axiosClient.get<Movie[]>("/todos")
+    return response.data;
 }
