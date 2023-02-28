843f652e1efefa7a9ad78cadca68022d6fbb1040
ChisKien <chiskien03214@gmail.com>
2023-02-28 16:26:11 +0700

    finish navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Navbar.scss";
-import {Container, Heading} from "@chakra-ui/react";
 
 export const Navbar: React.FC = () => {
+    const [isShowed, handleShow] = useState<boolean>(false);
+    const transitionNavbar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavbar);
+        return () => window.removeEventListener("scroll", transitionNavbar); //clean up code for scrolling
+    }, []);
+
     return (
-        <div className={"nav nav__black"}>
+        <div className={`nav ${isShowed && "nav__black"}`}>
             {/*<Heading as="h1" size={"4xl"}>Netflix</Heading>*/}
             <div className="nav__content">
                 <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
