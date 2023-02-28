8f6a291e9b540266bffe24c9ed55ad81d051997b
ChisKien <chiskien03214@gmail.com>
2023-02-28 15:03:08 +0700

    Global styling .scss

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import {Container, Heading} from "@chakra-ui/react";
 export const Navbar: React.FC = () => {
     return (
         <Container maxW={"100vw"} className="nav">
-            <Heading as="h1">Netflix</Heading>
+            <Heading as="h1" size={"4xl"}>Netflix</Heading>
             <div className="nav__content">
                 <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
-                     alt="Netflix logo"/>
+                     alt="Netflix logo" className={"nav__logo"}/>
                 <img src="~/../peep-101.svg" alt="avatar"/>
             </div>
         </Container>
