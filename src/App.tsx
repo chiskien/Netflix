dd30f04d4c803bd4eda17659469e3d3253a26373
ChisKien <chiskien03214@gmail.com>
2023-03-09 21:25:44 +0700

    Adding more link router in Navbar.scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Navbar} from "./components/Navbar/Navbar";
 import React from "react";
 import {Container} from "@chakra-ui/react";
 import {Banner} from "./components/Banner/Banner";
+import {Footer} from "./components/Footer/Footer";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Navbar/>
             <Banner/>
             <HomePage/>
+            <Footer/>
         </Container>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./Navbar.scss";
-import {Box, Button, Stack} from "@chakra-ui/react";
+import {Box, Link, Stack, Text} from "@chakra-ui/react";
 import {Link as ReactLink} from "react-router-dom";
-import {Link} from "@chakra-ui/react";
 
 export const Navbar: React.FC = () => {
     const [user, setUser] = useState<boolean>(false);
@@ -22,11 +21,23 @@ export const Navbar: React.FC = () => {
     return (
         <Box className={`nav ${isShowed && "nav__black"}`}>
             {/*<Heading as="h1" size={"4xl"}>Netflix</Heading>*/}
-            <Stack direction={"row"} className="nav__content">
-                <Box>
-                    <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
-                         alt="Netflix logo" className={"nav__logo"}/>
-                </Box>
+            <Stack direction={"row"} className="nav__content" justifyContent={"space-between"}>
+                <Stack width={"100%"} direction={"row"} alignItems={"center"} spacing={"20px"}>
+                    <Box>
+                        <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+                             alt="Netflix logo" className={"nav__logo"}/>
+                    </Box>
+                    <Stack direction={"row"} width={"70%"} spacing={"40px"} alignItems={"center"}>
+                        <Link to={"/"} as={ReactLink} color={"teal.50"} className={"nav__link"}>Home</Link>
+                        <Link to={"/movies"} as={ReactLink} color={"teal.50"} className={"nav__link"}>Movies</Link>
+                        <Link to={"/series"} as={ReactLink} color={"teal.50"} className={"nav__link"}>TV-Series</Link>
+                        <Link to={"/genre"} as={ReactLink} color={"teal.50"} className={"nav__link"}>Genre</Link>
+                        <Link to={"/country"} as={ReactLink} color={"teal.50"} className={"nav__link"}>Country</Link>
+                    </Stack>
+                    <Box>
+                        <Text>adasdsa</Text>
+                    </Box>
+                </Stack>
                 <Stack spacing={"10px"} direction={"row"} alignItems={"center"}>
                     {user ?
                         (<img src="~/../peep-101.svg" alt="avatar" className={"nav__user-logo"}/>)
